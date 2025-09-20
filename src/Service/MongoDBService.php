936f441103310b810ce3dc916c93aa710887c5cd
Nabil-FRA <nabil.ait.radi1997@gmail.com>
2025-09-21 01:25:14 +0200

    correction_controller4

--- src/Service/MongoDBService.php
+++ src/Service/MongoDBService.php
@@ -12,7 +12,10 @@ class MongoDBService
     public function __construct(string $uri, string $database)
     {
         $this->client = new Client($uri);
-        $this->database = $this->client->selectDatabase('EcoRide');
+        
+        // CORRECTION : Utiliser la variable $database passée en argument
+        // au lieu d'un nom codé en dur.
+        $this->database = $this->client->selectDatabase($database);
     }
 
     public function getDatabase()
